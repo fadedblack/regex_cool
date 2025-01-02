c53b8cdc25c49a94965b4db7802c91349f33f880
Inkeet C <inkeetcha234@gmail.com>
2025-01-02 22:37:52 +0530

    add digits in word

--- gpt_regexen.js
+++ gpt_regexen.js
@@ -17,3 +17,16 @@ Deno.test("Testing cat not in word", () => {
   assertFalse(specificWord.test("Akshay is on the caten"));
   assertFalse(specificWord.test("I love category theory."));
 });
+
+// Assignment 2: Matching Digits in a String
+const digitsInWord = /[0-9]+/;
+
+Deno.test("Testing digits in word", () => {
+  assert(digitsInWord.test("There are 3 cats in the house."));
+  assert(digitsInWord.test("There are 30 cats in the house."));
+  assert(digitsInWord.test("-30 is a negative number"));
+});
+
+Deno.test("Testing digits not in word", () => {
+  assertFalse(digitsInWord.test("No digits here!"));
+});
